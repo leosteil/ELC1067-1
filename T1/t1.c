cea2f2bcb9a08f27eea8cbdf95987bad36fe55f4
AnthonyTailer <aalmeida@inf.ufsm.br>
2015-03-12 21:10:37 -0300

    Finalizado 12/03 21:10

--- T1/t1.c
+++ T1/t1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void le_alunos(int* matriculas, char nomes[][50], int *n){
+void le_alunos(int* matriculasAluno, char nomes[][50], int *n){
 	int mat;
 	char c;
 	char nome[50];
@@ -22,51 +22,62 @@ void le_alunos(int* matriculas, char nomes[][50], int *n){
 			c=fgetc(f);			
 		}
 		        nome[i]='\0';//delimita o final de cada linha
-		        matriculas[linha]= mat;//armazena todas as matriculas no vetor
+		        matriculasAluno[linha]= mat;//armazena todas as matriculas no vetor
 			strcpy(nomes[linha],nome);
 			linha++;			
 	}
-	        *n = linha;//quantas linhas a busca deve percorrer
+	        *n = linha;//quantas linhas a busca deve percorrer no arquivo
 	        fclose(f);
         }
 }
 
-void le_notas(int* matriculas){
-        float n1,n2,media;
+void le_notas(int* matriculasNota, float* media){
+        float n1,n2;
+        int linha =0,mat;
         FILE *f = fopen("notas.txt","r");
 	if(f == NULL){
 	        printf("Não foi possível abrir o arquivo ou o mesmo se encontra vazio\n");
 	}else{
-	        while(feof(f)){}  
+	        while(feof(f) == 0){
+	                if(fscanf(f,"%d %f %f",&mat,&n1,&n2) < 0)//testar se o arquivo terminou EOF = -1 e pega os dados do FILE 	
+			        break;  
+		        matriculasNota[linha]= mat;//armazena todas as matriculas no vetor
+		        media[linha] = (n1 + n2)/2;//calcula as médias e armazena no vetor 
+		        linha++;  
+	        }  
                 
+        }
 }
 
-void busca(int* n, char nomes[][50], char* p, int* matriculas){
-  int i=0;
-  
-        for(i=0;i<*n;i++){ 
-             if(strstr(nomes[i],p) != NULL){
-               printf("%d %s\n",matriculas[i],nomes[i]);  
+void media_alunos(int* n, char* busca, char nomes[][50], int* matriculasAlunos, int* matriculasNota, float* media){
+  int i=0,j;
+        for(i=0;i<*n;i++){
+                j=0; 
+             if(strstr(nomes[i],busca) != NULL){//avalia se contém algum nome relacionado com a busca
+                while(matriculasAlunos[i] != matriculasNota[j]){//verifica se a matricula é a mesma até achar
+                        j++;
+             }
+                printf("Nome: %s Media: %.3f\n",nomes[i],media[j]); //imprime o vetor de medias correspondente ao nome do aluno
         }  
     }     
 } 
 
 
-int main(int argc, char**argv){
+int main(int argc, char**argv){//pega a busca em linha de comando
 	char nome[50];
-	char* p = nome;
+	char* busca = nome;
 	if(argc > 1){
-		p = argv[1];
-}
-	
-	int matriculas[50];
+		busca = argv[1];
+}	
+	int matriculasAluno[50];
+	int matriculasNota[50];
 	char nomes[50][50];
+	float media[50];
 	int n;
-	float notas[50][50];
 	
-	le_alunos(matriculas,nomes,&n);
-	busca(&n,nomes,p,matriculas); 
+	le_alunos(matriculasAluno,nomes,&n);
+	le_notas(matriculasNota,media);
+	media_alunos(&n,busca,nomes,matriculasAluno,matriculasNota,media); 
 
 	return 0;
-
 }
